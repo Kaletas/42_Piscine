f50f25d10685594b0bff1035a2dff6dc81d320f1
Kaletas <bkaleta64@gmail.com>
2024-09-10 13:07:32 +0200

    update

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 12:49:52 by bkaleta           #+#    #+#             */
+/*   Updated: 2024/09/10 13:04:15 by bkaleta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_sort_int_tab(int *tab, int size);
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	index;
+
+	index = 0;
+	while (index < size - 1)
+	{
+		if (tab[index] > tab[index + 1])
+		{
+			temp = tab[index];
+			tab[index] = tab[index + 1];
+			tab[index + 1] = temp;
+			index = -1;
+		}
+		index++;
+	}
+}
+
+// int	main(void)
+// {
+// 	int	tab[] = {42, -1337, 17, 54, 13, 80085, 2137};
+// 	int	size = 7;
+// 	int	i = 0;
+
+
+// 	printf("BEFORE SORT:\n");
+// 	while (tab[i])
+// 	{
+// 		printf("%d ", tab[i]);
+// 		i++;
+// 	}
+// 	printf("\n");
+// 	ft_sort_int_tab(tab, size);
+// 	i = 0;
+// 	printf("AFTER SORT:\n");
+// 	while (tab[i])
+// 	{
+// 		printf("%d ", tab[i]);
+// 		i++;
+// 	}
+// 	printf("\n");
+// 	return (0);
+// }
