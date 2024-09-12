29f510d9ed9e958863ec21a6531f765b15e780fe
Kaletas <bkaleta64@gmail.com>
2024-09-12 22:15:48 +0200

    update

---
+++ C03/ex01/ft_strncmp.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 13:37:33 by bkaleta           #+#    #+#             */
+/*   Updated: 2024/09/12 13:38:52 by bkaleta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	(void)n;
+	int	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
