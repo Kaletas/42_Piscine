43d9447bea9893d2a668861874aa815afd27f0f5
Kaletas <bkaleta64@gmail.com>
2024-09-12 13:14:56 +0200

    Update

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,8 +6,35 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 00:13:56 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/12 00:14:17 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/12 12:36:15 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlen(char *str);
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	src_count;
+	unsigned int	index;
+
+	src_count = ft_strlen(src);
+	index = 0;
+	while (src[index] && index < (size - 1))
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (src_count);
+}
--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,8 +6,49 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 00:14:43 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/12 00:14:50 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/12 13:10:12 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putstr_non_printable(char *str);
+int		ft_str_is_printable(char letter);
+void	ft_putchar_hexa(char letter);
+
+void	ft_putchar_hexa(char letter)
+{
+	char	*hex;
+	char	hex_output[2];
+
+	hex = "0123456789abcdef";
+	hex_output[0] = hex[letter / 16];
+	hex_output[1] = hex[letter % 16];
+	write(1, hex_output, 2);
+}
+
+int	ft_str_is_printable(char letter)
+{
+	if (letter >= 32 && letter <= 126)
+		return (1);
+	else
+		return (0);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_str_is_printable(str[i]) == 0)
+		{
+			write(1, "\\", 1);
+			ft_putchar_hexa(str[i]);
+		}
+		else if (ft_str_is_printable(str[i]) == 1)
+			write(1, &str[i], 1);
+		i++;
+	}
+}
