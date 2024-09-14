520ecbe9483af4c79eb3d6bc8706fdae9a102e21
Kaletas <bkaleta64@gmail.com>
2024-09-14 16:23:00 +0200

    added ex02 & ex03

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -6,8 +6,31 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 23:57:14 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/13 00:45:17 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/14 15:33:03 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putnbr(int nb);
+void	ft_putchar(long nbr);
+
+void	ft_putchar(long nbr)
+{
+	write(1, &nbr, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long	nbr;
+
+	nbr = (long)nb;
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr = -nbr;
+	}
+	if (nbr > 9)
+		ft_putnbr(nbr / 10);
+	ft_putchar(nbr % 10 + '0');
+}
--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,8 +6,32 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 23:57:34 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/12 23:57:40 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/14 16:21:15 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(char *str);
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while (*str == ' ' || *str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str)
+	{
+		if (*str >= '0' && *str <= '9')
+			result = result * 10 + (*str - '0');
+		else
+			break ;
+		str++;
+	}
+	return (result * sign);
+}
