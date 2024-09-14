62135173ad734e528103f2d3cf1e310069378084
Kaletas <bkaleta64@gmail.com>
2024-09-15 00:35:37 +0200

    update

--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -6,8 +6,12 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 23:58:17 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/12 23:58:24 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/15 00:35:00 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi_base(char *str, char *base);
+
+// zrobic funkcje do sprawdzenia czy znak ze str == jakis znak z bazy
+// zrobic licznik znakow z bazy do mnozenia resulta
+// "0123456789abcdef" 
