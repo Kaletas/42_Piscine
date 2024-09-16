b087133456678bcea664e86472c66b7469f9a440
Kaletas <bkaleta64@gmail.com>
2024-09-17 01:10:10 +0200

    added ft_is_prime

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -6,8 +6,28 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 11:50:55 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/15 11:51:06 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/17 01:09:47 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_prime(int nb);
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	i = 3;
+	while (i <= (nb / 2))
+	{
+		if (nb % i == 0)
+			return (0);
+		i += 2;
+	}
+	return (1);
+}
