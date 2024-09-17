132142ef192b35de90dbed2e131b68d217ba6897
Kaletas <bkaleta64@gmail.com>
2024-09-18 00:14:29 +0200

    update

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,7 +6,73 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 11:54:32 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/15 11:54:33 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/18 00:13:30 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int		ft_strcmp(char *s1, char *s2);
+void	ft_putstr(char *str);
+void	ft_bubblesort(char **a, char **b);
+void	ft_sort_params(int ac, char **av);
+
+void	ft_sort_params(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = 1;
+		while (j < ac - 1)
+		{
+			if (ft_strcmp(av[j], av[j + 1]) > 0)
+				ft_bubblesort(&av[j], &av[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_bubblesort(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+
+	if (ac > 1)
+		ft_sort_params(ac, av);
+	i = 1;
+	while (i < ac)
+	{
+		ft_putstr(av[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
