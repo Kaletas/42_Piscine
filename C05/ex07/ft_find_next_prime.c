83f37e90db8a0b44e7fa0875e4dc5ed16f083ad2
Kaletas <bkaleta64@gmail.com>
2024-09-17 09:44:55 +0200

    update

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,8 +6,39 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 11:51:23 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/15 11:51:32 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/17 09:37:26 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_find_next_prime(int nb);
+int	ft_is_prime(int nb);
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	i = 3;
+	while (i <= (nb / 2))
+	{
+		if (nb % i == 0)
+			return (0);
+		i += 2;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	next_prime;
+
+	next_prime = nb;
+	while (!ft_is_prime(next_prime))
+		next_prime++;
+	return (next_prime);
+}
