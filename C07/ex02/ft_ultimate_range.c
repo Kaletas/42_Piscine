be3c37c029248dd529fc9cdda2946e2c8c127f8a
Kaletas <bkaleta64@gmail.com>
2024-09-18 19:36:08 +0200

    update

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -6,8 +6,34 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 00:28:26 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/18 00:28:33 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/18 18:12:40 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 int	ft_ultimate_range(int **range, int min, int max);
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	count;
+	int	i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	count = max - min;
+	*range = malloc(sizeof(int) * count);
+	if (!*range)
+		return (-1);
+	i = 0;
+	while (min < max)
+	{
+		(*range)[i] = min;
+		i++;
+		min++;
+	}
+	return (count);
+}
