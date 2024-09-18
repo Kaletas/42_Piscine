f1fc5abc251578c926ca9185f0740e05af319f6f
Kaletas <bkaleta64@gmail.com>
2024-09-18 16:15:02 +0200

    update

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -6,8 +6,39 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 00:27:00 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/18 00:27:04 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/18 16:14:35 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 char	*ft_strdup(char *src);
+int		ft_strlen(char *src);
+
+int	ft_strlen(char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	dup = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (!dup)
+		return (0);
+	while (src[i])
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
