7262076707072dd9dc9997549d2de711eea00140
Kaletas <bkaleta64@gmail.com>
2024-09-20 14:51:09 +0200

    update

--- C08/ex02/ft_abs.h
+++ C08/ex02/ft_abs.h
@@ -6,7 +6,15 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 14:11:05 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/20 14:11:06 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/20 14:50:51 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_ABS_H
+# define FT_ABS_H
+
+#define ABS(Value) ((Value) < 0 ? -(Value) : (Value))
+
+int	ft_abs(int value);
+
+#endif
--- C08/ex03/ft_point.h
+++ C08/ex03/ft_point.h
@@ -6,7 +6,20 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 14:11:09 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/09/20 14:11:10 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/09/20 14:44:20 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+
+void	set_point(t_point *point);
+
+#endif
